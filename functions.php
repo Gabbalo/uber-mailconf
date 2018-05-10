e92e23ccb82d27f8df5098f36fa129e3cdaf6107
Gabriel <Gabbalo@users.noreply.github.com>
2018-05-10 07:50:19 +0200

    Update functions.php
    Write some (pseudo) code fragments just to remember them

--- functions.php
+++ functions.php
@@ -1,1 +1,22 @@
+function randomstring () {
+  // create random chars to uniquify addesses
+  return $random;
+}
 
+function writeqmail ($catchallprefix) {
+  // write qmail file based on database entrys
+  $file = fopen(".qmail-$catchallprefix-default, "w") or die ("Unable to open file!");
+  // generate $content
+  fwrite($file, $content);
+  fclose($file);
+}
+
+function installdatabase () {
+  // Get credentials from user and store them in config-file
+  // CREATE database and tables
+}
+
+function connectdatabase () {
+  // Import config-file with credentials
+  // Connect to database
+}
